2c41d39b4fdbf9bcefa6af2e8a83b7904d97b47d
LarsonRagnar <larionov1992@gmail.com>
2024-02-10 17:33:44 +0300

    minimalCode

--- src/main/java/org/example/task1/task2/Main.java
+++ src/main/java/org/example/task1/task2/Main.java
@@ -6,22 +6,18 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {3,2,10,4,3};
-        Map<Integer,Integer> map = new HashMap<>();
-        boolean bl=false;
-        bl = getFlag(arr, map, bl);
-        System.out.println(bl);
-
+        System.out.println(getFlag(arr));
     }
 
-    private static boolean getFlag(int[] arr, Map<Integer, Integer> map, boolean bl) {
+    private static boolean getFlag(int[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             if(map.containsKey(arr[i])){
-                bl =true;
-                break;
+                return true;
             }
             map.put(arr[i],1);
 
         }
-        return bl;
+        return false;
     }
 }
